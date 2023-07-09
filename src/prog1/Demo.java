b634c464405aedbb7a5c809c269a73cbfe87c047
Uditdas10 <uditdashalisahar@gmail.com>
2023-07-09 13:30:30 +0530

    Third commit

--- src/prog1/Demo.java
+++ src/prog1/Demo.java
@@ -4,9 +4,9 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello java..");
+		//System.out.println("Hello java..");
 		System.out.println("Now uploaded in GitHub..");
-	
+	    System.out.println("Again Git Push");
 
 	}
 
